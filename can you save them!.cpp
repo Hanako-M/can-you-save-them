93dc72a58962f332a5ae3b512d8f61ddecff78fa
hana <Dell@DESKTOP-716KVUJ>
2024-04-09 15:00:02 +0200

    fixed background

--- can you save them!.cpp
+++ can you save them!.cpp
@@ -11,6 +11,9 @@ float const left_wall = 0;
 
 static const float view_height = 800;
 
+// Function to handle level transition
+
+
 
 
 struct player
@@ -72,6 +75,74 @@ struct player
 
     }
 };
+
+void handleLevelTransition(Sprite& background, player& player1, const Vector2f& originalPlayerPosition, const Texture& levelTexture) {
+    // Change the background to level 2
+    background.setTexture(levelTexture);
+    background.setScale(1.2f, 1.2f);
+    background.setPosition(0.f, -40.f);
+
+    // Reset player's position
+    player1.rect.left = originalPlayerPosition.x;
+    player1.rect.top = originalPlayerPosition.y;
+}
+
+struct enemyhana {
+    Sprite sprite;
+    int speed;
+    float animation = 0;
+    FloatRect rect;
+    float currentframe;
+    float move_x;
+    bool movingRight; // Flag to indicate the direction of movement
+
+    void initial_values(Texture& playerTexture)
+    {
+        sprite.setTexture(playerTexture);
+        sprite.setScale(2.0, 2.0);
+        move_x = 0;
+        currentframe = 0;
+        movingRight = true; // Start moving to the right initially
+
+        // Set initial position
+        rect.left = 900; // Start position x = 500
+        rect.top = 700;  // Adjust as needed
+    }
+
+    void update(float timer)
+    {
+        
+        // Update position
+        rect.left += move_x * timer;
+
+        // Switch direction if reaching the boundaries
+        if (rect.left >= 800)
+            movingRight = false;
+        else if (rect.left <= 500)
+            movingRight = true;
+
+        // Update animation
+        currentframe += 0.005 * timer;
+        if (currentframe > 8)
+        {
+            currentframe -= 8;
+        }
+
+        // Set sprite texture based on direction
+        if (move_x > 0)
+            sprite.setTextureRect(IntRect(48 * int(currentframe), 0, 48, 48));
+        else if (move_x < 0)
+            sprite.setTextureRect(IntRect(48 * int(currentframe) + 48, 0, -48, 48));
+
+        sprite.setPosition(rect.left, rect.top);
+        move_x = 0;
+    }
+};
+
+enemyhana hyena;
+
+
+
 struct Button {
     sf::Text text;
 
@@ -103,7 +174,7 @@ int main() {
     cout << " game team" << endl;
 
     RenderWindow window(VideoMode(1634, 1080), "SFML Window");
-
+    window.setFramerateLimit(60);
 
     // Load the splash texture
     Texture splashTexture, level1texture;
@@ -219,6 +290,15 @@ void Game_Play(RenderWindow& window)
     Sprite ob1(ob1tex);
     ob1.setScale(0.2, 0.2);
     ob1.setPosition(300, 750);
+
+
+    Texture hyenatext;
+    hyenatext.loadFromFile("Hyena_walk.png");
+
+    RectangleShape rectangle(sf::Vector2f(100.f, 100.f)); 
+    rectangle.setPosition(6400.f, 800.f); // Position (x=300, y=200)
+    rectangle.setFillColor(sf::Color::White); // Fill color
+   
     /*Texture attack_Texture;
     attack_Texture.loadFromFile("Attack_1.png");
 
@@ -237,16 +317,33 @@ void Game_Play(RenderWindow& window)
     dead_Texture.loadFromFile("Dead.png");
 
     */
-
+    enemyhana hyena;
+    hyena.initial_values(hyenatext);
+    hyena.sprite.setTextureRect(IntRect(0, 0, 48, 48));
+    hyena.rect.left = 10;
 
     player player1;
     player1.initial_values(playerTexture);
     player1.sprite.setTextureRect(IntRect(0, 0, 128, 128));
     player1.rect.left = 10;
     player1.rect.top = 500;
-    Vector2f playerPosition(50, 60); // Initial position
+    Vector2f playerPosition(10, 500); // Initial position
+
+    Texture level2texture;
+    level2texture.loadFromFile("level2_background.png");
 
 
+
+    /*for (int i = 0; i < 3; i++) {
+        cacodemon[i].sprite.setTexture(cacotext);
+        cacodemon[i].speed = rand() % (6 - 1 + 1) + 1;
+        cacodemon->sprite.setScale(2.5, 2.5);
+        int y = rand() % (1500 - 90 + 1) + 90;
+
+        cacodemon[i].sprite.setPosition(0, y);
+        cacodemon[i].sprite.setTextureRect(IntRect(0,0,85,64));
+    }*/
+    hyena.speed = 1; // Set the speed of the enemy
 
     Clock clock;
     int posCNT = 1, bgINX = 0;
@@ -266,6 +363,11 @@ void Game_Play(RenderWindow& window)
         timer /= 1000;
         if (timer > 100)
             timer = 100;
+
+        // Handle level transition
+        if (player1.sprite.getGlobalBounds().intersects(rectangle.getGlobalBounds())) {
+            handleLevelTransition(background, player1, playerPosition, level2texture);
+        }
 
         if (Keyboard::isKeyPressed(Keyboard::D))
         {
@@ -302,13 +404,40 @@ void Game_Play(RenderWindow& window)
             cam.move(-5, 0); // Move the view to the left
         }
 
+        
 
         player1.update(timer);
+
+       /* for (int i = 0; i < 3; i++) {
+            cacodemon[i].animation += 0.03;
+            cacodemon[i].sprite.move(cacodemon[i].speed, 0);
+
+            if (cacodemon[i].sprite.getPosition().x < 0) {
+                int y = rand() % (9) + 8;
+                cacodemon[i].sprite.setPosition(0, y);
+                cacodemon[i].speed = rand() % (6 - 1 + 1) + 1;
+            }
+
+            // Calculate the correct animation frame index, wrapping around if necessary
+            int frameWidth = 85;
+            int maxFrames = 512 / frameWidth; // Total frames in the animation
+            cacodemon[i].animation = fmod(cacodemon[i].animation, maxFrames);
+
+            cacodemon[i].sprite.setTextureRect(IntRect(frameWidth * int(cacodemon[i].animation), 0, frameWidth, 64));
+        }*/
+        float dt = clock.restart().asSeconds();
+
+        hyena.update(dt);
 
         window.clear();
         window.draw(background);
         window.draw(ob1);
         window.draw(player1.sprite);
+        //window.draw(hyena.sprite);
+        window.draw(rectangle);
+        /*for (int i = 0; i < 3; i++) {
+            window.draw(cacodemon[i].sprite);
+        }*/
         window.display();
 
 
